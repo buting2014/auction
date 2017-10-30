ebcf08c97619dc633d8ae9df9b670f672c151fcb
buting <butingting@weidian.com>
2017-10-30 11:34:15 +0800

    [fix]接口请求token修改

--- src/service/request.js
+++ src/service/request.js
@@ -3,41 +3,25 @@
  */
 import wepy from 'wepy';
 let methods = 'get delete head options post put patch';
-const SERVER_DOMAIN = 'http://api.pivotal-china.com';
 
 let request = {};
 methods.split(' ').forEach(method => {
   request[method] = (url, params) => new Promise((resolve, reject) => {
+    params.token = wept.getStorage('token');
+    params.id = wepy.getStorage('id');
     wepy.request({
-      url: url,
+      url: url, // 仅为示例，并非真实的接口地址
       data: params,
       header: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded' // 默认值
       },
       method: method,
-      timeout: 10000
-    }).then(response => {
-      const successCodes = '100000 100204'.split(' ');
-
-      if (successCodes.some(code => response.code === +code)) {
-        if (Object.prototype.toString.call(response.result) === '[object Object]' || Array.isArray(response.result)) {
-          let data = response.result;
-          resolve(data);
-        }
+      success: res => {
+        res.data.code === 100000 && resolve(res.data);
+      },
+      fail: err => {
+        reject(err)
       }
-      // if (response.code === 100001) {
-      //   window.location.href = `${SERVER_DOMAIN}/auth/wechat?redirect_uri=${window.location.href}`;
-      //   reject(response);
-      // } else if (response.code === 100401) {
-      //   window.location.href = `${SERVER_DOMAIN}/auth/wechat?redirect_uri=${window.location.href}`;
-      // } else if (response.code === 100006) {
-      //   let img = <img src={`https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=${response.result.ticket}`}/>;
-      //   // Modal.alert('请长按以下二维码关注公众号', img);
-      // } else {
-      //   reject(response);
-      // }
-    }).catch(err => {
-      console.log(JSON.stringify(err));
     });
   });
 });
