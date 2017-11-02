5cfd3edef2c5ecfb24ea9348f752473fd5be4d69
buting <butingting@weidian.com>
2017-11-02 12:17:00 +0800

    [fix] login

--- src/pages/list/data.js
+++ src/pages/list/data.js
@@ -4,19 +4,7 @@ let data = {
   qrcodeImg: '',
   wxId: '',
   tabList: ['未发布', '未开始', '进行中', '已完成', '流拍'],
-  auctionData: [{
-    image: '/images/slide-temp.png',
-    title: 'Iphone5',
-    descript: 'Iphone5特卖',
-    joinNum: 8,
-    price: '125'
-  },
-  {
-    image: '/images/slide-temp.png',
-    title: 'Iphone5',
-    descript: 'Iphone5特卖',
-    joinNum: 8,
-    price: '125'
-  }]
+  auctionData: [],
+  activeTab: '未发布'
 };
 export default data;
