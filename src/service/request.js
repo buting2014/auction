c300b3ab229183bece87ba0980b812f04b90bc0b
buting <butingting@weidian.com>
2017-11-06 10:10:32 +0800

    [fix] request.js

--- src/service/request.js
+++ src/service/request.js
@@ -8,28 +8,31 @@ let request = {};
 methods.split(' ').forEach(method => {
   request[method] = (url, params) => new Promise((resolve, reject) => {
     !params && (params = {});
-    if (!wepy.getStorageSync('ifLogin')) {
-      wxlogin(() => {
-        params.token = wepy.getStorageSync('token');
-        params.id = wepy.getStorageSync('userId');
-        wepy.request({
-          url: url, // 仅为示例，并非真实的接口地址
-          data: params,
-          header: {
-            'Content-Type': 'application/json' // 默认值
-          },
-          method: method,
-          success: res => {
-            res.data.code === 100000 && resolve(res.data.result);
-          },
-          fail: err => {
-            reject(err)
-          }
-        });
+    // wepy.setStorageSync('ifLogin', false)
+    let requestFun = () => {
+      params.token = wepy.getStorageSync('token');
+      params.id = wepy.getStorageSync('userId');
+      wepy.request({
+        url: url, // 仅为示例，并非真实的接口地址
+        data: params,
+        header: {
+          'Content-Type': 'application/json' // 默认值
+        },
+        method: method,
+        success: res => {
+          res.data.code === 100000 && resolve(res.data.result);
+        },
+        fail: err => {
+          reject(err)
+        }
       });
+    };
+    if (!wepy.getStorageSync('ifLogin')) {
+      wxlogin(requestFun);
     } else {
-      
+      requestFun();
     }
   });
 });
+
 export default request;
